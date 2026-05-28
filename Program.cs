432930aa1843c2c3f65a803c860d6372aeb024c9
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2026-05-28 12:49:36 +0200

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Shiko.FaqService.Data;
 using Shiko.FaqService.Endpoints;
-using Shiko.FaqService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,8 +35,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.UseHttpsRedirection();
-app.MapFaqEndpoints();
 app.UseCors("AllowFrontend");
+app.MapFaqEndpoints();
 
 app.Run();
 
